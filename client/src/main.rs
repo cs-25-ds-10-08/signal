4b2754f95a1465835258b95a4c0f8942d3a62e06
Mikkel-Boje <mikkel.larsen@stofanet.dk>
2025-03-17 10:09:33 +0100

    Added a break such that we can force registration earlier than the experiments

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,13 +1,13 @@
 use client::Client;
-use dotenv::dotenv;
+use dotenv::{dotenv, var};
 use futures::{future::join_all, stream::FuturesUnordered, StreamExt};
 use libsignal_core::ServiceId;
 use rand::{rngs::OsRng, seq::SliceRandom, Rng};
 use server::SignalServer;
 use std::{
-    env::{self, var},
+    env::{self},
     error::Error,
-    fs,
+    fs, io,
     path::{Path, PathBuf},
     sync::Arc,
     time::Duration,
@@ -26,6 +26,8 @@ mod socket_manager;
 mod storage;
 #[cfg(test)]
 mod test_utils;
+
+type Clients = Vec<Arc<RwLock<Client<Device, SignalServer>>>>;
 
 fn client_db_path() -> String {
     fs::canonicalize(PathBuf::from("./client_db".to_string()))
@@ -86,7 +88,7 @@ fn get_server_info() -> (Option<String>, String) {
     }
 }
 
-async fn make_clients(client_amount: usize) -> Vec<Arc<RwLock<Client<Device, SignalServer>>>> {
+async fn make_clients(client_amount: usize) -> Clients {
     let (cert_path, server_url) = get_server_info();
     let mut clients = vec![
         Arc::new(RwLock::new(
@@ -175,10 +177,8 @@ async fn receive_message(client: &mut Client<Device, SignalServer>) -> (ServiceI
 }
 
 // Random noise
-async fn experiment_1(rounds: usize, client_amount: usize) -> Result<(), Box<dyn Error>> {
-    dotenv()?;
-
-    let clients = Arc::new(make_clients(client_amount).await);
+async fn experiment_1(rounds: usize, clients: Clients) -> Result<(), Box<dyn Error>> {
+    let clients = Arc::new(clients);
 
     clients
         .iter()
@@ -207,7 +207,7 @@ async fn experiment_1(rounds: usize, client_amount: usize) -> Result<(), Box<dyn
 
                     if true_by_chance(5) {
                         let random_client_nr = loop {
-                            let rand = OsRng.gen_range(0..client_amount);
+                            let rand = OsRng.gen_range(0..clients.len());
                             if rand != i {
                                 break rand;
                             }
@@ -237,14 +237,11 @@ async fn experiment_1(rounds: usize, client_amount: usize) -> Result<(), Box<dyn
 // A client will only be present in one group
 async fn experiment_2(
     rounds: usize,
-    client_amount: usize,
+    clients: Clients,
     group_size_min: usize,
     group_size_max: usize,
 ) -> Result<(), Box<dyn Error>> {
-    dotenv()?;
-
-    let clients = make_clients(client_amount).await;
-    let groups = make_groups(client_amount, group_size_min, group_size_max);
+    let groups = make_groups(clients.len(), group_size_min, group_size_max);
 
     groups
         .iter()
@@ -294,6 +291,16 @@ fn true_by_chance(chance: usize) -> bool {
     OsRng.gen_range(0..=100) <= chance
 }
 
+async fn init(client_amount: usize) -> Clients {
+    dotenv().expect("You need to add a .env file");
+    let clients = make_clients(client_amount).await;
+
+    println!("Ready: press any key to continue");
+    let _ = io::stdin().read_line(&mut String::new());
+    println!("Starting Experiment");
+    clients
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     const ROUNDS: usize = 100;
@@ -304,6 +311,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     #[allow(dead_code)]
     const GROUP_SIZE_MAX: usize = 5;
 
-    experiment_1(ROUNDS, CLIENT_AMOUNT).await
-    //experiment_2(ROUNDS, CLIENT_AMOUNT, GROUP_SIZE_MIN, GROUP_SIZE_MAX).await
+    let clients = init(CLIENT_AMOUNT).await;
+
+    experiment_1(ROUNDS, clients).await
+    //experiment_2(ROUNDS, clients, GROUP_SIZE_MIN, GROUP_SIZE_MAX).await;
 }
